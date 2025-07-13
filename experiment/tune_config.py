905486af1baa4479a08e5af4cf65c5e90a74fe03
Szabó Péter Arvid <szabo.arvid1025@gmail.com>
2025-07-13 17:17:10 +0200

    results random seed

--- experiment/tune_config.py
+++ experiment/tune_config.py
@@ -9,7 +9,7 @@ content_root = os.path.abspath('.')
 config_default = {'scenario': 'class',
                   'runs': 1,
                   'runs_val': 2,  # To increase the confidence of best_config
-                  'seed': 1234,
+                  'seed': 1234, #4444, 1234,
                   'verbose': True,
                   'path_prefix': content_root + '/result/tune_and_exp',
                   'fix_order': False,
