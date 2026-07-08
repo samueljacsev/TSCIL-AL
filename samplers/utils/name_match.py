b1ecc315fc7136f58f2883c4e66b740b7abcf05d
Samuel Jacsev <jacsev.samuel@gmail.com>
2026-07-08 18:24:15 +0200

    remove test samplers

--- samplers/utils/name_match.py
+++ samplers/utils/name_match.py
@@ -2,23 +2,15 @@
 from samplers.random_iter import RandomIterSampler
 from samplers.uncertainty import UncertaintySampler
 from samplers.typi_clust import TypiClustSampler
-from samplers.uncertainty_diversity import UncertaintyDiversitySampler
-from samplers.tcud import TypiClustUncertaintyDiversitySampler
-from samplers.kmeans_ppp import KMeansPPPSampler            
+from samplers.uncertainty_diversity import UncertaintyDiversitySampler         
 from samplers.coreset import CoreSetSampler
-from samplers.coreset_prob import CoreSetProbSampler
 from samplers.typicore import TypiCoreSampler
-from samplers.typi_kmeans import TypiKmeansSampler
 
 samplers = {
     'random_iter': RandomIterSampler,
     'uncertainty': UncertaintySampler,
     'typi_clust': TypiClustSampler,
     'uncertainty_div': UncertaintyDiversitySampler,
-    'tcud': TypiClustUncertaintyDiversitySampler,
-    'kmeans_ppp': KMeansPPPSampler,
     'coreset': CoreSetSampler,
-    'coreset_prob': CoreSetProbSampler,
-    'typi_kmeans': TypiKmeansSampler,
     'typicore': TypiCoreSampler
 }
